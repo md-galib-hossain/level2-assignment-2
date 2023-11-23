514b6bb7bac43684c1ba287fab19b11c8cd68abd
md-galib-hossain <mdgalib23@gmail.com>
2023-11-23 16:27:04 +0600

    Server fully initiated

--- dist/server.js
+++ dist/server.js
@@ -13,14 +13,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const port = 5000;
-let server;
-//my server function
-function bootstrap() {
+require("dotenv").config();
+const port = process.env.PORT;
+const db_url = process.env.DATABASE_URL;
+const mongoose_1 = __importDefault(require("mongoose"));
+function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        server = app_1.default.listen(port, () => {
-            console.log(`Example app listening on port ${port}`);
-        });
+        try {
+            yield mongoose_1.default.connect(db_url);
+            //my server
+            app_1.default.listen(port, () => {
+                console.log(`Example app listening on port ${port}`);
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
     });
 }
-bootstrap();
+main();
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
-import { Server } from "http"
 import app from "./app"
-const port = 5000
+require("dotenv").config();
+const port = process.env.PORT 
+const db_url = process.env.DATABASE_URL
+import mongoose from "mongoose";
+async function main() {
+    try{
 
-let server : Server 
+        await mongoose.connect(db_url as string);
+    
+    //my server
+   app.listen(port
+        , () => {
+            console.log(`Example app listening on port ${port
+            }`)
+          })
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+  
+  }
+
+main()
 
 
-//my server function
-async function bootstrap () {
 
-   server =  app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-      })
-
-}
-bootstrap()
 
